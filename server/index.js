5ea5eee7e6e206e187f19a89f9078ce9e6cef231
nishita-esrat <nishitaesrat120@gmail.com>
2024-01-01 11:18:07 +0600

    created route for getting single chef data

--- server/index.js
+++ server/index.js
@@ -4,13 +4,19 @@ let app = express();
 const data = require("./data.json");
 
 app.use(cors());
-
+// for checking sever is running or not
 app.get("/health", (_req, res) => {
   res.json({ msg: "server is running" });
 });
-
+// for getting all chef data
 app.get("/all_chef", (_req, res) => {
   res.send(data);
+});
+// for getting single chef data depend on chef id
+app.get("/all_chef/:chefId", (req, res) => {
+  const chefId = parseInt(req.params.chefId)
+  const result = data.filter((item) => item.chefId == chefId);
+  res.send(result);
 });
 
 app.listen(800, () => {
