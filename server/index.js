d953fcfb47321ccbb225f2f4a555b5bf25d74e3a
nishita-esrat <nishitaesrat120@gmail.com>
2024-01-03 12:20:01 +0600

    created route for getting favourite chef items

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 let app = express();
 const data = require("./data.json");
 
-
+app.use(express.json());
 app.use(cors());
 // for checking sever is running or not
 app.get("/health", (_req, res) => {
@@ -15,9 +15,25 @@ app.get("/all_chef", (_req, res) => {
 });
 // for getting single chef data depend on chef id
 app.get("/all_chef/:chefId", (req, res) => {
-  const chefId = parseInt(req.params.chefId)
+  const chefId = parseInt(req.params.chefId);
   const result = data.find((item) => item.chefId == chefId);
   res.send(result);
+});
+// for getting favourite chef item
+app.post("/all_fav_item", (req, res) => {
+  const storageFavItems = req.body;
+  let favItems = [];
+  for (const id in storageFavItems) {
+    data.map((item) => {
+      item.recipes.find((item) => {
+        if (item.recipeId == id) {
+          favItems.push(item);
+          return;
+        }
+      });
+    });
+  }
+  res.send(favItems);
 });
 
 app.listen(800, () => {
