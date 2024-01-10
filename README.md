7979eae58ea45be508ce122ffaeff43db5dc6713
nishita-esrat <nishitaesrat120@gmail.com>
2024-01-10 23:16:44 +0600

    update reademe.md file

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 
 # chef
--[live link](https://chef-72883.web.app)
+- using react and firebase
+- for login , this web use firebase authentication
+- basic sever side using express and node
+- deploy client side firebase
+- deploy sever side vercel
+- [live link](https://chef-72883.web.app)
+
--- server/index.js
+++ server/index.js
@@ -5,28 +5,34 @@ const data = require("./data.json");
 
 app.use(express.json());
 app.use(cors());
+
 // for checking sever is running or not
 app.get("/health", (_req, res) => {
   res.json({ msg: "server is running" });
 });
+
 // for getting all chef data
 app.get("/all_chef", (_req, res) => {
   res.send(data);
 });
+
 // for getting single chef data depend on chef id
 app.get("/all_chef/:chefId", (req, res) => {
   const chefId = parseInt(req.params.chefId);
   const result = data.find((item) => item.chefId == chefId);
   res.send(result);
 });
+
 // for getting favourite chef item
 app.post("/all_fav_item", (req, res) => {
   const storageFavItems = req.body;
   let favItems = [];
   for (const id in storageFavItems) {
-    data.map((item) => {
-      item.recipes.find((item) => {
+    data.map((chef) => {
+      chef.recipes.find((item) => {
         if (item.recipeId == id) {
+          item["chefName"] = chef.chefName;
+          item["chefPhotoId"] = chef.chefPhotoId;
           favItems.push(item);
           return;
         }
